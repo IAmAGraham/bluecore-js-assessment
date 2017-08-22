1cbbd877ffb1dbb644690e3cb6b987d27d8f9ebf
github username <github email address>
2017-08-22 15:23:17 -0400

    final submission

--- questionThree.js
+++ questionThree.js
@@ -1,19 +1,19 @@
 function awesomeTree(height){
-  var top = "ø"
   var arr = [];
-  for (i=1; i<=height; i++){
-    var space = Array(height=i+1).join(' ');
-    arr.push(space + Array(2*1).join('*') + space);
-  }
-  return arr.join('\n');
+  if (arr === 0){
+    return null
+  } else if (arr === 1) {
+      return "*|*"
+  } else {
+      for (i=1; i<=height; i++){
+        var space = Array(height=i+1).join(' ');
+        arr.push(space + "*|" + Array(2*1).join('*') + "|*" + space);
+      }
+      return tree = "ø" + arr.join('\n');
+    }
 }
 
 
-
-function awesomeTree(height){
-  var top = "ø"
-  var firstBranch = "*|*"
-  if (height === 1){
-    var smallTree =
-  }
-}
+// Write a function, awesomeTree(height) that logs a tree of the correct height to the console.
+// For example, awesomeTree(5) should console.log() the following, as a single string:
+// Pad the output with spaces so each line is the same length, with the last line having no spaces.
